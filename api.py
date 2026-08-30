c8612ad928484fc9f9c96875893391ec22cdd5b9
TaskMoon <vaitahosinmmm@gmail.com>
2026-08-30 22:54:03 +0600

    Fix Flask startup

--- api.py
+++ api.py
@@ -15,7 +15,9 @@ from database import (
     update_admin_reply, close_support_chat
 )
 
-app = Fla@app.after_request
+app = Flask(__name__)
+
+@app.after_request
 def add_cors_headers(response):
     response.headers["Access-Control-Allow-Origin"] = "*"
     response.headers["Access-Control-Allow-Headers"] = "Content-Type"
