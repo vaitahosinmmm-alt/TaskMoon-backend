0b661caf3401ef592eb1bd2a6a9ae3ebfef083ed
TaskMoon <vaitahosinmmm@gmail.com>
2026-08-30 22:30:47 +0600

    Fix support chat CORS

--- api.py
+++ api.py
@@ -15,7 +15,12 @@ from database import (
     update_admin_reply, close_support_chat
 )
 
-app = Flask(__name__)
+app = Fla@app.after_request
+def add_cors_headers(response):
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
+    return response
 
 
 @app.get("/")
